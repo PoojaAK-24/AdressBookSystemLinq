2ff817adfe2fe387ae197334bead8a161046f5d9
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-08-17 20:41:19 +0530

    Add project files.

---
+++ Day35-AdressBookLinq/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day35_AdressBookLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome_To_AddressBookSystem_Linq");
+            AddrssBookModel model = new AddrssBookModel();
+            AddressBookDataTable dataTable = new AddressBookDataTable();
+            dataTable.CreateTable(model);
+        }
+    }
+}
